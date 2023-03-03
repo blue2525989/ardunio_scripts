a7a35cc2fa61a8f554c0b90017eb2a77d0b5e2a8
Jason Bierbrauer <jbierbrauer99@gmail.com>
2023-03-03 14:44:24 -0600

    Update blue_bot_controller.ino

--- blue_bot_controller.ino
+++ blue_bot_controller.ino
@@ -4,12 +4,35 @@
 
  String RC = "RIGHT_CLICK\n";
  String LC = "LEFT_CLICK\n";
+ String RELEASE = "RELEASE|";
+
+ const int F7_BUTTON = 9;
+ const int F8_BUTTON = 8;
+ const int F9_BUTTON = 7;
+ const int F10_BUTTON = 10;
+ const int F11_BUTTON = 16;
+ const int F12_BUTTON = 14;
+
+ int F7_BUTTON_STATE = 0;
+ int F8_BUTTON_STATE = 0;
+ int F9_BUTTON_STATE = 0;
+ int F10_BUTTON_STATE = 0;
+ int F11_BUTTON_STATE = 0;
+ int F12_BUTTON_STATE = 0;
+
 
 void setup() {  
   Serial.begin(9600);
   // initialize mouse and keyboard control:
   Mouse.begin();
   Keyboard.begin();
+  // setup physical buttons
+  pinMode(F7_BUTTON, INPUT);
+  pinMode(F8_BUTTON, INPUT);
+  pinMode(F9_BUTTON, INPUT);
+  pinMode(F10_BUTTON, INPUT);
+  pinMode(F11_BUTTON, INPUT);
+  pinMode(F12_BUTTON, INPUT);
 }
 
 void keyPress(int keyVal) {
@@ -18,7 +41,48 @@ void keyPress(int keyVal) {
 }
 
 void loop() {
+  // delay(1);
+
+
+  // F7_BUTTON_STATE = digitalRead(F7_BUTTON);
+  // F8_BUTTON_STATE = digitalRead(F8_BUTTON);
+  // F9_BUTTON_STATE = digitalRead(F9_BUTTON);
+  // F10_BUTTON_STATE = digitalRead(F10_BUTTON);
+  // F11_BUTTON_STATE = digitalRead(F11_BUTTON);
+  // F12_BUTTON_STATE = digitalRead(F12_BUTTON);
+  // Serial.println(F7_BUTTON_STATE);
+  // Serial.println(F8_BUTTON_STATE);
+  // Serial.println(F9_BUTTON_STATE);
+  // Serial.println(F10_BUTTON_STATE);
+  // Serial.println(F11_BUTTON_STATE);
+  // Serial.println(F12_BUTTON_STATE);
+  // Serial.println();
+  // if (F7_BUTTON_STATE == LOW ) {
+  //   Serial.println("f7");
+  //   // keyPress(KEY_F7);
+  // }
+  // if (F8_BUTTON_STATE == LOW) {
+  //   Serial.println("f8");
+  //   // keyPress(KEY_F8);
+  // }
+  // if (F9_BUTTON_STATE == LOW) {
+  //   Serial.println("f9");
+  //   // keyPress(KEY_F9);
+  // }
+  // if (F10_BUTTON_STATE == LOW) {
+  //   Serial.println("f10");
+  //   // keyPress(KEY_F10);
+  // }
+  // if (F11_BUTTON_STATE == LOW) {
+  //   Serial.println("f11");
+  //   // keyPress(KEY_F11);
+  // }
+  // if (F12_BUTTON_STATE == LOW) {
+  //   Serial.println("f12");
+  //   // keyPress(KEY_F12);
+  // }
   if (Serial.available() > 0) {
+
     const String inputStr = Serial.readStringUntil("\n");
     if (inputStr != NULL) {
       Serial.print("Incoming string-> ");
@@ -129,4 +193,7 @@ void loop() {
       }
     }
   }
+
+
+
 }
